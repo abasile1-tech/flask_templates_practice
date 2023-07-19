5513d754e07a8fbc4d7c7c0ab97f9264a757fee3
John <ashton.basile@gmail.com>
2023-07-19 12:03:09 +0100

    We added the ability to add a task

--- controllers/tasks_controller.py
+++ controllers/tasks_controller.py
@@ -1,8 +1,17 @@
-from flask import render_template, Blueprint
-from models.task_list import tasks
+from flask import render_template, Blueprint, request
+from models.task_list import tasks, add_new_task
+from models.task import Task
 
 tasks_blueprint = Blueprint("tasks", __name__)
 
 @tasks_blueprint.route('/tasks')
 def index():
     return render_template('index.jinja', title='My Task List', tasks=tasks)
+
+@tasks_blueprint.route('/tasks', methods=['POST'])
+def add_task():
+    task_title = request.form['title']
+    task_description = request.form['description']
+    new_task = Task(task_title, task_description, False)
+    add_new_task(new_task)
+    return render_template('index.jinja', title="My Task List", tasks=tasks)
